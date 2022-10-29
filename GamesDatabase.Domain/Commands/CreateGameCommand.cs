6b4090b6b19a160069707a926b0f4d182b6fc97b
Ismael Castro <Ismaelgabrijob@hotmail.com>
2022-10-29 16:41:24 -0300

    Criado o command para adicionar/criar um jogo na api

---
+++ GamesDatabase.Domain/Commands/CreateGameCommand.cs
@@ -0,0 +1,70 @@
+﻿using GamesDatabase.Domain.Commands.Contracts;
+using GamesDatabase.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesDatabase.Domain.Commands
+{
+    public class CreateGameCommand : ICommand
+    {
+        public CreateGameCommand() { }
+
+        public CreateGameCommand(string type,
+            string name,
+            string genre,
+            string description,
+            decimal initialPrice,
+            double discount_Percentage,
+            decimal finalPrice,
+            IList<string> screenshotPath)
+        {
+            Type = type;
+            Name = name;
+            Genre = genre;
+            Description = description;
+            InitialPrice = initialPrice;
+            Discount_Percentage = discount_Percentage;
+            FinalPrice = finalPrice;
+            ScreenshotPath = screenshotPath;
+        }
+
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public decimal InitialPrice { get; set; }
+        public double Discount_Percentage { get; set; }
+        public decimal FinalPrice { get; set; }
+        public IList<string> ScreenshotPath { get; set; }
+        public IDictionary<string, string> Notification { get; set; }
+
+        public bool Validate()
+        {
+            if(Type.Length < 3)
+                Notification.Add("Type", "O tipo deve conter ao menos 3 caracteres");
+
+            if (Genre.Length < 3)
+                Notification.Add("Genre", "O Genero deve conter ao menos que 3 caracteres");
+
+            if (Description == "" || Description == null)
+                Notification.Add("Description", "É preciso uma descrição");
+
+            if (InitialPrice == null)
+                Notification.Add("InitialPrice", "Preço inicial igual a nulo");
+
+            if (FinalPrice == null)
+                Notification.Add("FinalPrice", "Preço final igual a nulo");
+
+            if (ScreenshotPath == null)
+                Notification.Add("ScreenshotPath", "É preciso de pelo menos 1 screenshoot");
+
+            if (Notification.Count > 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ GamesDatabase.Domain/Commands/GenericCommandResult.cs
@@ -0,0 +1,15 @@
+﻿using GamesDatabase.Domain.Commands.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesDatabase.Domain.Commands
+{
+    public class GenericCommandResult : ICommandResult
+    {
+        public bool Success { get; set; }
+        public object Data { get; set; }
+    }
+}
